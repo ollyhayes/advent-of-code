be0fe2bf3a98f7c5e017ea8eaa6484545c1787a3
Olly Hayes <olly.hayes@gmail.com>
2020-11-08 17:12:14 +0100

    Day three part 3

--- src/2019/day3.py
+++ src/2019/day3.py
@@ -8,35 +8,6 @@ def parse_input(input_raw: str) -> List[List[Tuple[str, int]]]:
 		in input_raw.split("\n")
 	]
 
-world_radius = 250
-origin = world_radius, world_radius
-world = [[0] * world_radius * 2 for i in range(0, world_radius * 2)]
-
-def inc_point(world, position, value):
-	x, y = position
-	value = world[x][y] 
-	world[x][y] = value + 1
-	# print(f"set point: {x},{y} to {value + 1}")
-	return value + 1
-
-def plot(input: List[Tuple[str, int]], world) -> List[Tuple[int, int]]:
-	position = origin
-	inc_point(world, position, 1)
-	intersections_found: List[Tuple[int, int]] = []
-
-	for direction, length in input:
-		direction_parts = directions[direction]
-
-		for _ in range(0, length):
-			position = position[0] + direction_parts[0], position[1] + direction_parts[1]
-			new_value = inc_point(world, position, 1)
-
-			if new_value == 2:
-				print(f"intersection found: {position[0] - world_radius}, {position[1] - world_radius}")
-				intersections_found.append(position)
-	
-	return intersections_found
-
 directions: Dict[str, Tuple[int, int]] = {
 	"R": (1,0),
 	"L": (-1,0),
@@ -44,19 +15,20 @@ directions: Dict[str, Tuple[int, int]] = {
 	"D": (0,-1),
 }
 
-def plot2(input: List[Tuple[str, int]]) -> Set[Tuple[int, int]]:
-	occupied_positions: Set[Tuple[int, int]] = set()
+def plot2(input: List[Tuple[str, int]]) -> Dict[Tuple[int, int], int]:
+	occupied_positions: Dict[Tuple[int, int], int] = {}
 	position: Tuple[int, int] = (0, 0)
+	distance_travelled = 0
 
 	for direction, length in input:
 		direction_parts = directions[direction]
 
 		for _ in range(0, length):
 			position = position[0] + direction_parts[0], position[1] + direction_parts[1]
-			occupied_positions.add(position)
+			occupied_positions[position] = distance_travelled
+			distance_travelled += 1
 
 	return occupied_positions
-
 
 def compute_part_1(input: str) -> int:
 	inputs = parse_input(input)
@@ -64,10 +36,27 @@ def compute_part_1(input: str) -> int:
 	path1 = plot2(inputs[0])
 	path2 = plot2(inputs[1])
 
-	intersections = path1 & path2
+	intersections = path1.keys() & path2.keys()
 	distances = [abs(position[0]) + abs(position[1]) for position in intersections]
 
 	return min(distances)
+
+
+def compute_part_2(input: str) -> int:
+	inputs = parse_input(input)
+
+	path1 = plot2(inputs[0])
+	path2 = plot2(inputs[1])
+
+	intersection_points = path1.keys() & path2.keys()
+
+	distances = [
+		path1[intersection_point] + path2[intersection_point]
+		for intersection_point
+		in intersection_points
+	]
+
+	return min(distances) + 2
 
 def main() -> int:
 	dirname = os.path.dirname(__file__)
@@ -76,21 +65,9 @@ def main() -> int:
 		input = input_file.read()
 
 	print(f"part1: {compute_part_1(input)}")
-	# print(f"part2: {compute_part_2(input, 19690720)}")
+	print(f"part2: {compute_part_2(input)}")
 
 	return 0
 
 if __name__ == '__main__':
 	exit(main())
-
-# plot(test_input[0], world)
-# plot(test_input[1], world)
-
-# for line in world:
-# 	row = ""
-# 	for field in line:
-# 		row += str(field)
-
-# 	print(row)
-
-# would have been much easier to just build up a set of visited values, and then find the intersections
--- src/2019/day3_test.py
+++ src/2019/day3_test.py
@@ -1,5 +1,5 @@
 import pytest
-from day3 import compute_part_1
+from day3 import compute_part_1, compute_part_2
 
 @pytest.mark.parametrize(
 	("input", "expected"),
@@ -14,3 +14,17 @@ U98,R91,D20,R16,D67,R40,U7,R15,U6,R7""", 135),
 )
 def test_part_1(input: str, expected: int) -> None:
 	assert compute_part_1(input) == expected
+
+@pytest.mark.parametrize(
+	("input", "expected"),
+	(
+		("""R8,U5,L5,D3
+U7,R6,D4,L4""", 30),
+		("""R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83""", 610),
+		("""R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7""", 410),
+	)
+)
+def test_part_2(input: str, expected: int) -> None:
+	assert compute_part_2(input) == expected
