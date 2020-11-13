07efa11acf96e954dda0d3517a144e41d6108b8e
Olly Hayes <olly.hayes@gmail.com>
2020-11-13 07:59:15 +0100

    Tidy day4

--- src/2019/day4.py
+++ src/2019/day4.py
@@ -17,46 +17,17 @@ def is_valid(input: int) -> bool:
 			has_double = True
 
 	return has_double
-	
 
-def compute_part_1(input: str) -> int:
+def compute_part_1() -> int:
 	count = 0
 	for i in range(254032, 789860):
 		if is_valid(i):
 			count += 1
 	
 	return count
-	# inputs = parse_input(input)
-
-	# path1 = plot2(inputs[0])
-	# path2 = plot2(inputs[1])
-
-	# intersections = path1.keys() & path2.keys()
-	# distances = [abs(position[0]) + abs(position[1]) for position in intersections]
-
-	# return min(distances)
-
-
-def compute_part_2(input: str) -> int:
-	return 1
-	# inputs = parse_input(input)
-
-	# path1 = plot2(inputs[0])
-	# path2 = plot2(inputs[1])
-
-	# intersection_points = path1.keys() & path2.keys()
-
-	# distances = [
-	# 	path1[intersection_point] + path2[intersection_point]
-	# 	for intersection_point
-	# 	in intersection_points
-	# ]
-
-	# return min(distances) + 2
 
 def main() -> int:
-	print(f"part1: {compute_part_1(input)}")
-	print(f"part2: {compute_part_2(input)}")
+	print(f"part1: {compute_part_1()}")
 
 	return 0
 
