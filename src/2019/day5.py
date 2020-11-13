e0cc6bf110900884fe3f04ce23336680e23c533b
Olly Hayes <olly.hayes@gmail.com>
2020-11-13 08:30:38 +0100

    Add new op_codes, support param types and bring in input

--- src/2019/day5.py
+++ src/2019/day5.py
@@ -28,12 +28,30 @@ def op_2(index, program):
 
 	return index + 4, program
 
+def op_3(index, program):
+	return index + 2, program
+	left = get_referenced_value(index + 1, program)
+	right = get_referenced_value(index + 2, program)
+	value = left * right
+	pos = program[index + 3]
+
+	# print(f"multiply: {left} + {right} = {value} @ {pos}")
+
+	program[pos] = value
+
+	return index + 2, program
+
+def op_4(index, program):
+	return index + 2, program
+
 def op_99(index, program):
 	return len(program), program
 
 ops = {
 	1: op_1,
 	2: op_2,
+	3: op_3,
+	4: op_4,
 	99: op_99
 }
 
@@ -41,8 +59,10 @@ def compute_part_1(input: str) -> List[int]:
 	program = list(map(int, input.split(",")))
 	index = 0
 	while index < len(program):
-		op_code = program[index]
-		# print(f"i: {index}, op_code: {op_code}, program: {program[:10]}")
+		op_code_info = program[index]
+		op_code = int(str(op_code_info)[-2:])
+		param_info = int(str(op_code_info)[0:-2] or 0)
+		print(f"i: {index}, op_code: {op_code}, param_info: {param_info}, program: {program[:10]}")
 		operation = ops[op_code]
 		index, program = operation(index, program)
 
@@ -54,7 +74,7 @@ def compute_part_2(input: str) -> int:
 
 def main() -> int:
 	dirname = os.path.dirname(__file__)
-	filename = os.path.join(dirname, "day3_input.txt")
+	filename = os.path.join(dirname, "day5_input.txt")
 	with open(filename, "r") as input_file:
 		input = input_file.read()
 
