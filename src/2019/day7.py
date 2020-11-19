be5848730ebc0e9bc0c97a94b97653d80a9b3f4e
Olly Hayes <olly.hayes@gmail.com>
2020-11-19 21:44:01 +0100

    Day7 part one working

--- src/2019/day7.py
+++ src/2019/day7.py
@@ -137,44 +137,46 @@ def compute_output_signal(input_program: str, sequence: List[int]) -> int:
 
 		run_program(program, str(i), input, output)
 		current_signal = output.read()
+
+		while current_signal == 0:
+			current_signal = output.read()
+		
+		print(f"completed iteration {i}, signal = {current_signal}")
+		print()
 	
 	return current_signal
 
-def compute_part_1(input: str) -> None:
-	print(compute_output_signal(input, [1, 2, 3, 4, 5]))
+def compute_part_1(input: str) -> int:
+	output = 0
+
+	for i in range(0, 3125):
+		phase_attempt = convert_to_base(i, 5)
+		if len(set(phase_attempt)) == len(phase_attempt):
+			output = max(output, compute_output_signal(input, [int(char) for char in phase_attempt]))
+	
+	return output
+
+def convert_to_base(input: int, base: int) -> str:
+	value = ""
+	while input:
+		value = str(input % base) + value
+		input //= 5
+
+	return value.zfill(5)
 
 def compute_part_2(input: str) -> int:
 	return 1
 
-# def main() -> int:
-# 	dirname = os.path.dirname(__file__)
-# 	filename = os.path.join(dirname, "day5_input.txt")
-# 	with open(filename, "r") as input_file:
-# 		input = input_file.read()
+def main() -> int:
+	dirname = os.path.dirname(__file__)
+	filename = os.path.join(dirname, "day7_input.txt")
+	with open(filename, "r") as input_file:
+		input = input_file.read()
 
-# 	compute_part_1(input)
-# 	# print(f"part2: {compute_part_2(input)}")
+	print(f"total maximum: {compute_part_1(input)}")
+	# print(f"part2: {compute_part_2(input)}")
 
-# 	return 0
+	return 0
 
-# if __name__ == '__main__':
-# 	fd = sys.stdin.fileno()
-# 	old = termios.tcgetattr(fd)
-# 	tty.setcbreak(fd)
-
-# 	try:
-# 		main()
-# 	finally:
-# 		termios.tcsetattr(fd, termios.TCSADRAIN, old)
-
-# 	exit(0)
-
-	# while True:
-	# 	line = sys.stdin.read(1)
-	# 	print(f'Processsssing Message from sys.stdin *****{line}*****')
-
-	# 	if line == "q":
-	# 		print('exiting')
-	# 		break
-
-	# tty.setraw(stdin_fd)
+if __name__ == '__main__':
+	exit(main())
