1d1024deafa8096a59282d889b73cfa6299b2db4
Olly Hayes <olly.hayes@gmail.com>
2020-12-02 18:41:11 +0100

    Extract get_hits

--- src/2019/day10.py
+++ src/2019/day10.py
@@ -53,36 +53,41 @@ def get_angle(position: Tuple[int, int]) -> float:
 
 	return angle + (pi / 2 if position[0] > 0 else 3 * pi / 2)
 
+def calculate_hits(rows: List[str], x_pos: int, y_pos: int, height: int, width: int) -> Dict[float, List[Tuple[int, int]]]:
+	hits: Dict[float, List[Tuple[int, int]]] = {}
+
+	for y_tar in range(0, height):
+		for x_tar in range(0, width):
+			if rows[y_tar][x_tar] == ".":
+				continue
+
+			x_diff = x_tar - x_pos
+			y_diff = y_tar - y_pos
+
+			if x_diff == 0 and y_diff == 0:
+				continue
+
+			# direction = simplify_fraction((x_diff, y_diff))
+			direction = get_angle((x_diff, y_diff))
+
+			hits[direction] = hits.get(direction, []) + [(x_diff, y_diff)]
+
+	return hits
+
 def compute_part_1(input: str) -> Tuple[Tuple[int, int], int]:
 	rows = input.split("\n")
-	WIDTH = len(rows[0])
-	HEIGHT = len(rows)
+	width = len(rows[0])
+	height = len(rows)
 
 	max_asteroids_detected = 0
 	best_position: Tuple[int, int] = (-1,-1)
 
-	for y_pos in range(0, HEIGHT):
-		for x_pos in range(0, WIDTH):
+	for y_pos in range(0, height):
+		for x_pos in range(0, width):
 			if rows[y_pos][x_pos] == ".":
 				continue
 
-			hits: Dict[float, List[Tuple[int, int]]] = {}
-
-			for y_tar in range(0, HEIGHT):
-				for x_tar in range(0, WIDTH):
-					if rows[y_tar][x_tar] == ".":
-						continue
-
-					x_diff = x_tar - x_pos
-					y_diff = y_tar - y_pos
-
-					if x_diff == 0 and y_diff == 0:
-						continue
-
-					# direction = simplify_fraction((x_diff, y_diff))
-					direction = get_angle((x_diff, y_diff))
-
-					hits[direction] = hits.get(direction, []) + [(x_diff, y_diff)]
+			hits = calculate_hits(rows, x_pos, y_pos, width, height)
 			
 			astroids_detected = len(hits)
 
