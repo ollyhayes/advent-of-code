91e3e759f906d453b78f30ae8cba765c9d5713a7
Olly Hayes <olly.hayes@gmail.com>
2020-12-04 13:36:59 +0100

    Finished day 4

--- src/2020/day4.py
+++ src/2020/day4.py
@@ -1,51 +1,83 @@
 from math import floor
 from typing import List, Tuple
 import os
+import re
 
 def compute_part_1(input: str) -> int:
 	# lines = list(map(int, input.split("\n")))
 	input_split = input.split("\n\n")
 
-	# fields = [field.split(":") for field in passport.replace("\n", " ").split(" ") for passport in passports]
-
 	passports = [{field.split(":")[0]: field.split(":")[1] for field in passport.replace("\n", " ").split(" ")} for passport in input_split]
 
+	hair = re.compile('^#[0-9a-f]{6}$')
+	eye_colours = {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'}
+	pid = re.compile('^[0-9]{9}$')
+
 	required_fields = {
-    "byr", # (Birth Year)
-    "iyr", # (Issue Year)
-    "eyr", # (Expiration Year)
-    "hgt", # (Height)
-    "hcl", # (Hair Colour)
-    "ecl", # (Eye Colour)
-    "pid", # (Passport ID)
-   #  "cid" # (Country ID)
+		"byr": lambda value: 1920 <= int(value) <= 2002, # (Birth Year)
+		"iyr": lambda value: 2010 <= int(value) <= 2020, # (Issue Year)
+		"eyr": lambda value: 2020 <= int(value) <= 2030, # (Expiration Year)
+		"hgt": lambda value: value[-2:] in {"cm", "in"} and (150 <= int(value[:-2]) <= 193) if value[-2:] == "cm" else (59 <= int(value[:-2]) <= 76), # (Height)
+		"hcl": lambda value: hair.match(value), # (Hair Colour)
+		"ecl": lambda value: value in eye_colours , # (Eye Colour)
+		"pid": lambda value: pid.match(value), # (Passport ID)
+		#  "cid" # (Country ID)
 	}
 
-	invalid_passports = []
+	valid_passports = []
 	for passport in passports:
-		if not required_fields <= passport.keys():
-			invalid_passports.append(passport)
 
-	return len(invalid_passports)
+		is_valid = required_fields.keys() <= passport.keys()
 
-def add(x, y, width):
-	return (x[0] + y[0]) % width, x[1] + y[1]
+		if not is_valid:
+			continue
 
-# def compute_part_2(input: str) -> int:
-# 	slopes = [
-# 		(1, 1),
-# 		(3, 1),
-# 		(5, 1),
-# 		(7, 1),
-# 		(1, 2),
-# 	]
-# 	total_trees_multiplied = 1
+		for key, validator in required_fields.items():
+			try:
+				if not validator(passport[key]):
+					is_valid = False
+			except:
+				is_valid = False
 
-# 	for slope in slopes:
-# 		trees = compute_part_1(input, slope)
-# 		total_trees_multiplied *= trees
+		if is_valid:
+			valid_passports.append(passport)
+		
 
-# 	return total_trees_multiplied
+	return len(valid_passports)
+
+def test():
+	hair = re.compile('#[0-9a-f]{6}')
+	eye_colours = {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'}
+	pid = re.compile('[0-9]{9}')
+
+	required_fields = {
+		"byr": lambda value: 1920 <= int(value) <= 2002, # (Birth Year)
+		"iyr": lambda value: 2010 <= int(value) <= 2020, # (Issue Year)
+		"eyr": lambda value: 2020 <= int(value) <= 2030, # (Expiration Year)
+		"hgt": lambda value: value[-2:] in {"cm", "in"} and (150 <= int(value[:-2]) <= 193) if value[-2:] == "cm" else (59 <= int(value[:-2]) <= 76), # (Height)
+		"hcl": lambda value: len(hair) == 7 and hair.match(value), # (Hair Colour)
+		"ecl": lambda value: value in eye_colours , # (Eye Colour)
+		"pid": lambda value: len(value) == 9 and pid.match(value), # (Passport ID)
+		#  "cid" # (Country ID)
+	}
+
+	assert required_fields['byr']("2002")
+	assert not required_fields['byr']("2003")
+
+	assert required_fields['hgt']("60in")
+	assert required_fields['hgt']("190cm")
+	assert not required_fields['hgt']("190in")
+	assert not required_fields['hgt']("190")
+
+	assert required_fields['hcl']("#123abc")
+	assert not required_fields['hcl']("#123abz")
+	assert not required_fields['hcl']("123abc")
+
+	assert required_fields['ecl']("brn")
+	assert not required_fields['ecl']("wat")
+
+	assert required_fields['pid']("000000001")
+	assert not required_fields['pid']("0123456789")
 
 def main() -> int:
 	dirname = os.path.dirname(__file__)
@@ -54,7 +86,7 @@ def main() -> int:
 		input = input_file.read()
 
 	print(compute_part_1(input))
-	# print(compute_part_2(input))
+	# test()
 
 	return 0
 
