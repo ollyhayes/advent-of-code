fa7964c654b276204ac222c678eeb462ca31d361
Olly Hayes <olly.hayes@gmail.com>
2020-12-12 11:32:26 +0100

    Tidy up day 12

--- src/2020/day12.py
+++ src/2020/day12.py
@@ -2,16 +2,16 @@ from math import floor, sin, cos, pi, sqrt, atan
 from typing import Dict, List, Tuple, Optional, Callable
 import os
 
-def add(x: Tuple[int, int], y: Tuple[int, int]):
+def add(x: Tuple[float, float], y: Tuple[float, float]):
 	return x[0] + y[0], x[1] + y[1]
 
-def subtract(x: Tuple[int, int], y: Tuple[int, int]):
+def subtract(x: Tuple[float, float], y: Tuple[float, float]):
 	return x[0] - y[0], x[1] - y[1]
 
-def multiply(x: Tuple[int, int], y: int):
+def multiply(x: Tuple[float, float], y: float):
 	return x[0] * y, x[1] * y
 
-def get_angle(position: Tuple[int, int]) -> float:
+def calculate_angle(position: Tuple[float, float]) -> float:
 	if position[0] == 0:
 		return -pi / 2 if position[1] < 0 else pi / 2
 	
@@ -19,30 +19,26 @@ def get_angle(position: Tuple[int, int]) -> float:
 
 	return angle if position[0] > 0 else angle + pi
 
-	# return angle
+def calculate_vector(direction: float, value: float) -> Tuple[float, float]:
+	return value * cos(direction), value * sin(direction)
 
-
-def get_position_diff(direction: int, value: int) -> Tuple[int, int]:
-	rad = direction * pi / 180
-	return round(value * cos(rad)), round(value * sin(rad))
-
-def compute_part_1(input: str) -> int:
-	# rows = list(map(int, input.split("\n")))
+def compute_part_1(input: str) -> float:
+	# rows = list(map(float, input.split("\n")))
 	rows = input.split("\n")
 	position, direction = (0,0), 0
 
-	def move_in_direction(position_diff: Tuple[int,int]):
-		def move_inner(position: Tuple[int, int], direction: int, value: int) -> Tuple[Tuple[int, int], int]:
+	def move_in_direction(position_diff: Tuple[float,float]):
+		def move_inner(position: Tuple[float, float], direction: float, value: float) -> Tuple[Tuple[float, float], float]:
 			return add(position, multiply(position_diff, value)), direction
 		return move_inner
 
-	def turn(direction_diff: int):
-		def turn_inner(position: Tuple[int, int], direction: int, value: int) -> Tuple[Tuple[int, int], int]:
-			return position, direction + direction_diff * value
+	def turn(direction_diff: float):
+		def turn_inner(position: Tuple[float, float], direction: float, value: float) -> Tuple[Tuple[float, float], float]:
+			return position, direction + direction_diff * (value * pi / 180)
 		return turn_inner
 
-	def go_forward(position: Tuple[int, int], direction: int, value: int) -> Tuple[Tuple[int, int], int]:
-		return add(position, get_position_diff(direction, value)), direction
+	def go_forward(position: Tuple[float, float], direction: float, value: float) -> Tuple[Tuple[float, float], float]:
+		return add(position, calculate_vector(direction, value)), direction
 
 	ops: Dict[str, Callable] = {
 		"N": move_in_direction((0, -1)),
@@ -56,36 +52,36 @@ def compute_part_1(input: str) -> int:
 
 	for row in rows:
 		command = row[:1]
-		value = int(row[1:])
+		value = float(row[1:])
 
 		position, direction = ops[command](position, direction, value)
 
 		# print(f"row: {row}, pos: {position}, dir: {direction}")
 
-	return abs(position[0]) + abs(position[1])
+	return round(abs(position[0]) + abs(position[1]))
 
-def compute_part_2(input: str) -> int:
-	# rows = list(map(int, input.split("\n")))
+def compute_part_2(input: str) -> float:
+	# rows = list(map(float, input.split("\n")))
 	rows = input.split("\n")
 	position, waypoint_position = (0,0), (10, -1)
 
-	def move_in_direction(position_diff: Tuple[int,int]):
-		def move_inner(position: Tuple[int, int], waypoint_position: Tuple[int,int], value: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+	def move_in_direction(position_diff: Tuple[float,float]):
+		def move_inner(position: Tuple[float, float], waypoint_position: Tuple[float,float], value: float) -> Tuple[Tuple[float, float], Tuple[float, float]]:
 			return position, add(waypoint_position, multiply(position_diff, value))
 		return move_inner
 
-	def turn(direction_diff: int):
-		def turn_inner(position: Tuple[int, int], waypoint_position: Tuple[int, int], value: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
-			angle_to_waypoint = round(get_angle(waypoint_position) * 180 / pi)
-			distance_to_waypoint = round(sqrt(waypoint_position[0] * waypoint_position[0] + waypoint_position[1] * waypoint_position[1]))
+	def turn(direction_diff: float):
+		def turn_inner(position: Tuple[float, float], waypoint_position: Tuple[float, float], value: float) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+			angle_to_waypofloat = calculate_angle(waypoint_position)
+			distance_to_waypoint = sqrt(waypoint_position[0] * waypoint_position[0] + waypoint_position[1] * waypoint_position[1])
 
-			new_angle_to_paypoint = angle_to_waypoint + direction_diff * value
+			new_angle_to_paypofloat = angle_to_waypofloat + direction_diff * (value * pi / 180)
 
-			return position, get_position_diff(new_angle_to_paypoint, distance_to_waypoint)
+			return position, calculate_vector(new_angle_to_paypofloat, distance_to_waypoint)
 
 		return turn_inner
 
-	def go_forward(position: Tuple[int, int], waypoint_position: Tuple[int, int], value: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+	def go_forward(position: Tuple[float, float], waypoint_position: Tuple[float, float], value: float) -> Tuple[Tuple[float, float], Tuple[float, float]]:
 		diff = multiply(waypoint_position, value)
 
 		return add(position, diff), waypoint_position
@@ -102,15 +98,15 @@ def compute_part_2(input: str) -> int:
 
 	for row in rows:
 		command = row[:1]
-		value = int(row[1:])
+		value = float(row[1:])
 
 		position, waypoint_position = ops[command](position, waypoint_position, value)
 
-		print(f"row: {row}, pos: {position}, way: {waypoint_position}")
+		# print(f"row: {row}, pos: {position}, way: {waypoint_position}")
 
-	return abs(position[0]) + abs(position[1])
+	return round(abs(position[0]) + abs(position[1]))
 
-def main() -> int:
+def main() -> float:
 	input_filename = f"{__file__.split('.')[0]}_input.txt"
 	with open(input_filename, "r") as input_file:
 		input = input_file.read()
@@ -121,18 +117,4 @@ def main() -> int:
 	return 0
 
 if __name__ == '__main__':
-	# test_pos = [
-	# 	(1,0),
-	# 	(2,1),
-	# 	(1,1),
-	# 	(1,2),
-	# 	(0,1),
-	# 	(-1,1),
-	# 	(-1,0),
-	# 	(-1,-1),
-	# 	(0,-1),
-	# 	(1,-1),
-	# ]
-	# print([get_position_diff(int(get_angle(pos) * 180 / pi), 10) for pos in test_pos])
-	# exit(0)
 	exit(main())
