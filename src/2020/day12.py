cb545ed9608f15f0d2b4fd91b0ec4c2dab7a6d39
Olly Hayes <olly.hayes@gmail.com>
2020-12-12 15:01:42 +0100

    Tidy day 12

--- src/2020/day12.py
+++ src/2020/day12.py
@@ -2,25 +2,30 @@ from math import floor, sin, cos, pi, sqrt, atan
 from typing import Dict, List, Tuple, Optional, Callable
 import os
 
-def add(x: Tuple[float, float], y: Tuple[float, float]):
+Vector = Tuple[float, float]
+
+def add(x: Vector, y: Vector) -> Vector:
 	return x[0] + y[0], x[1] + y[1]
 
-def subtract(x: Tuple[float, float], y: Tuple[float, float]):
+def subtract(x: Vector, y: Vector) -> Vector:
 	return x[0] - y[0], x[1] - y[1]
 
-def multiply(x: Tuple[float, float], y: float):
+def multiply(x: Vector, y: float) -> Vector:
 	return x[0] * y, x[1] * y
 
-def calculate_angle(position: Tuple[float, float]) -> float:
-	if position[0] == 0:
-		return -pi / 2 if position[1] < 0 else pi / 2
+def calculate_angle(vector: Vector) -> float:
+	if vector[0] == 0:
+		return -pi / 2 if vector[1] < 0 else pi / 2
 	
-	angle = atan(position[1] / position[0])
+	angle = atan(vector[1] / vector[0])
 
-	return angle if position[0] > 0 else angle + pi
+	return angle if vector[0] > 0 else angle + pi
 
-def calculate_vector(direction: float, value: float) -> Tuple[float, float]:
-	return value * cos(direction), value * sin(direction)
+def calculate_vector(direction: float, magnitude: float) -> Vector:
+	return magnitude * cos(direction), magnitude * sin(direction)
+
+def magnitude(vector: Vector) -> float:
+	return sqrt(vector[0] * vector[0] + vector[1] * vector[1])
 
 def compute_part_1(input: str) -> float:
 	# rows = list(map(float, input.split("\n")))
@@ -28,16 +33,16 @@ def compute_part_1(input: str) -> float:
 	position, direction = (0,0), 0
 
 	def move_in_direction(position_diff: Tuple[float,float]):
-		def move_inner(position: Tuple[float, float], direction: float, value: float) -> Tuple[Tuple[float, float], float]:
+		def move_inner(position: Vector, direction: float, value: float) -> Tuple[Vector, float]:
 			return add(position, multiply(position_diff, value)), direction
 		return move_inner
 
 	def turn(direction_diff: float):
-		def turn_inner(position: Tuple[float, float], direction: float, value: float) -> Tuple[Tuple[float, float], float]:
+		def turn_inner(position: Vector, direction: float, value: float) -> Tuple[Vector, float]:
 			return position, direction + direction_diff * (value * pi / 180)
 		return turn_inner
 
-	def go_forward(position: Tuple[float, float], direction: float, value: float) -> Tuple[Tuple[float, float], float]:
+	def go_forward(position: Vector, direction: float, value: float) -> Tuple[Vector, float]:
 		return add(position, calculate_vector(direction, value)), direction
 
 	ops: Dict[str, Callable] = {
@@ -63,17 +68,17 @@ def compute_part_1(input: str) -> float:
 def compute_part_2(input: str) -> float:
 	# rows = list(map(float, input.split("\n")))
 	rows = input.split("\n")
-	position, waypoint_position = (0,0), (10, -1)
+	position, waypoint_vector = (0,0), (10, -1)
 
 	def move_in_direction(position_diff: Tuple[float,float]):
-		def move_inner(position: Tuple[float, float], waypoint_position: Tuple[float,float], value: float) -> Tuple[Tuple[float, float], Tuple[float, float]]:
-			return position, add(waypoint_position, multiply(position_diff, value))
+		def move_inner(position: Vector, waypoint_vector: Tuple[float,float], value: float) -> Tuple[Vector, Vector]:
+			return position, add(waypoint_vector, multiply(position_diff, value))
 		return move_inner
 
 	def turn(direction_diff: float):
-		def turn_inner(position: Tuple[float, float], waypoint_position: Tuple[float, float], value: float) -> Tuple[Tuple[float, float], Tuple[float, float]]:
-			angle_to_waypofloat = calculate_angle(waypoint_position)
-			distance_to_waypoint = sqrt(waypoint_position[0] * waypoint_position[0] + waypoint_position[1] * waypoint_position[1])
+		def turn_inner(position: Vector, waypoint_vector: Vector, value: float) -> Tuple[Vector, Vector]:
+			angle_to_waypofloat = calculate_angle(waypoint_vector)
+			distance_to_waypoint = magnitude(waypoint_vector)
 
 			new_angle_to_paypofloat = angle_to_waypofloat + direction_diff * (value * pi / 180)
 
@@ -81,10 +86,10 @@ def compute_part_2(input: str) -> float:
 
 		return turn_inner
 
-	def go_forward(position: Tuple[float, float], waypoint_position: Tuple[float, float], value: float) -> Tuple[Tuple[float, float], Tuple[float, float]]:
-		diff = multiply(waypoint_position, value)
+	def go_forward(position: Vector, waypoint_vector: Vector, value: float) -> Tuple[Vector, Vector]:
+		diff = multiply(waypoint_vector, value)
 
-		return add(position, diff), waypoint_position
+		return add(position, diff), waypoint_vector
 
 	ops: Dict[str, Callable] = {
 		"N": move_in_direction((0, -1)),
@@ -100,9 +105,9 @@ def compute_part_2(input: str) -> float:
 		command = row[:1]
 		value = float(row[1:])
 
-		position, waypoint_position = ops[command](position, waypoint_position, value)
+		position, waypoint_vector = ops[command](position, waypoint_vector, value)
 
-		# print(f"row: {row}, pos: {position}, way: {waypoint_position}")
+		# print(f"row: {row}, pos: {position}, way: {waypoint_vector}")
 
 	return round(abs(position[0]) + abs(position[1]))
 
